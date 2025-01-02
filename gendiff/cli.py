af2e8b6a6dccd48ab7fcb7ac839578071d984746
pro0om <phartsuk@gmail.com>
2025-01-02 11:34:55 +0200

    add parser.py, fix cli.py

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,7 +6,8 @@ def parse_args():
 
     parser.add_argument('firs_file',help='')
     parser.add_argument('second_file',help='')
-    parser.add_argument('-f','--format', help='set format to output', default='')
+    parser.add_argument('-f','--format', help='Set format to output',
+                         default='', type=str)
     parser.add_argument('--verbose',help='Print more info.', action='store_const', const=1)
 
     args = parser.parse_args()
