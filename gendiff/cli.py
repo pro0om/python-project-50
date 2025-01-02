4b8d9dbf4ea65d09f1c25b84507f0ebb66582642
pro0om <phartsuk@gmail.com>
2025-01-02 11:38:16 +0200

    fix

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -8,7 +8,6 @@ def parse_args():
     parser.add_argument('second_file',help='')
     parser.add_argument('-f','--format', help='Set format to output',
                          default='', type=str)
-    parser.add_argument('--verbose',help='Print more info.', action='store_const', const=1)
 
     args = parser.parse_args()
 
