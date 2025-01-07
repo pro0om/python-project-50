85ef9375fbd99113b344ad08c04a76b05f4243d0
pro0om <phartsuk@gmail.com>
2025-01-07 09:05:31 +0200

    add stylish

--- gendiff/formats/choice_format.py
+++ gendiff/formats/choice_format.py
@@ -1,8 +1,10 @@
 from gendiff.formats.json import format_diff_json
-
+from gendiff.formats.stylish import format_diff_stylish
 
 def format_diff(diff, format):
     if format == 'json':
         return format_diff_json(diff)
+    if format == 'stylish':
+        return format_diff_stylish(diff)
     else:
         raise ValueError(f"Unsupported formatter: {format}")
