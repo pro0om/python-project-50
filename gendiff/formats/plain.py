c488af6b839cd6a4801c4703edc4bb55673f2728
pro0om <phartsuk@gmail.com>
2025-01-10 14:22:18 +0200

    add plain.py code

---
+++ gendiff/formats/plain.py
@@ -0,0 +1,50 @@
+def to_str(value):
+    if isinstance(value, (list, dict)):
+        return '[dict]'
+    elif isinstance(value, bool):
+        return str(value).lower()
+    elif isinstance(value, int):
+        return value
+    elif value is None:
+        return 'null'
+    else:
+        return str(value)
+
+def make_plain_result_item(item, path=''):
+    current_key = item.get('name')
+    current_path = f"{path}.{current_key}" if path else current_key
+    action = item.get('action')
+    new_value = to_str(item.get('new_value'))
+    old_value = to_str(item.get('old_value'))
+
+    if action == 'added':
+        if path == '':
+            return f"Property {current_path} was added"
+        return f"Property in {path} was added {current_key} with value: {new_value}"
+    elif action == 'deleted':
+        if path == '':
+            return f"Property {current_path} was removed"
+        return f"Property in {path} was removed {current_key} "
+    elif action == 'modified':
+        return (
+            f"Property in {current_path} was updated {current_key}: "
+            f"from {old_value} to  {new_value}"
+        )
+    elif action == 'nested':
+        children = item.get('children')
+        return make_plain_result(children, current_path)
+    else:
+        return None
+
+
+def make_plain_result(diff, path=''):
+    result = []
+    for item in diff:
+        formatted_item = make_plain_result_item(item, path)
+        if formatted_item is not None:
+            result.append(formatted_item)
+    return '\n'.join(result)
+
+
+def format_diff_plain(data):
+    return make_plain_result(data)
