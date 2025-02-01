29548e4eb74ee12433a89818d1b9401aed6ac817
pro0om <phartsuk@gmail.com>
2025-02-01 13:40:33 +0200

    fix

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,18 +1,5 @@
 import json
 
 
-def json_value(data):
-    if not isinstance(data, dict):
-        if data == '':
-            return None
-        else:
-            return data
-    for key, value in data.items():
-        value = json_value(value)
-        data[key] = value
-    return data
-
-
-def format_diff_json(diff_result):
-    result = json_value(diff_result)
-    return json.dumps(result)
+def format_diff_json(diff):
+    return json.dumps(diff, indent=2, separators=(',', ': '))
